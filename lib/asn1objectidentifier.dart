0192b1c60441936e254fe3f768505e06e7e04be9
amaurel <alexandre.maurel@gmail.com>
2014-03-11 22:06:06 +0100

    rsa private key support

--- lib/asn1objectidentifier.dart
+++ lib/asn1objectidentifier.dart
@@ -3,9 +3,19 @@ part of asn1lib;
 
 class ASN1ObjectIdentifier  extends ASN1Object  {
   
+  List<int> oi;
+  
+  ASN1ObjectIdentifier(this.oi):super(tag:OBJECT_IDENTIFIER);
+  
   ASN1ObjectIdentifier.fromBytes(Uint8List bytes) {
       _encodedBytes = bytes;
       _initFromBytes();
-//      intValue = decodeInteger(this.valueBytes());
   }
+  
+  Uint8List encode() {
+      valueByteLength  = oi.length;
+      super._encodeHeader();
+      _setValueBytes(oi);
+      return _encodedBytes;
+    }
 }
