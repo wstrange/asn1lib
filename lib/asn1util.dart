74a45b8dc98a4a509f1b26ce3705077474c7b05c
Steven Roose <stevenroose@gmail.com>
2014-03-27 23:41:57 +0100

    Removed duplicate Util methods

--- lib/asn1util.dart
+++ lib/asn1util.dart
@@ -2,33 +2,6 @@
 part of asn1lib;
 
 
-String listToHexString(List<int> list) {
-
-  StringBuffer b = new StringBuffer("[");
-  bool doComma = false;
-  list.forEach( (v) {
-    if( v != null) {
-      if( doComma ) {
-        b.write(", ");
-      } else {
-        doComma = true;
-      }
-      //b.add("0x");
-      b.write((v <= 0xf ? "0" : ""));
-      b.write( v.toRadixString(16));
-    }
-  });
-  b.write("]");
-  return b.toString();
-}
-
-String hex(dynamic x) {
-  if( x is List)
-    return listToHexString(x);
-  else
-    return x.toRadixString(16);
-}
-
 class ASN1Util {
 
   // convert a list to a hex string. Used for debugging ASN1 output
