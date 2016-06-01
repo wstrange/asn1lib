347fe8108473f764960a89d8e0762cd2dcb057dd
Andreas Reiter <andreas.reiter@iaik.tugraz.at>
2016-06-01 17:47:32 +0200

    Always create BigInteger when reading ASN1Integer type, Int64List is not supported by dart2js

--- lib/asn1integer.dart
+++ lib/asn1integer.dart
@@ -110,6 +110,9 @@ class ASN1Integer extends ASN1Object {
 
   static dynamic decodeInteger(Uint8List bytes, {int offset: 0}) {
 
+    return new BigInteger(bytes.sublist(offset));
+
+    /* Int64 ist not supported by dart2js
     if (bytes.length - offset > 8) return new BigInteger(bytes.sublist(offset));
     var pad = (bytes[offset] & 0x80) == 0 ? 0 : 0xFF;
     var t = new Int64List(1);
@@ -131,6 +134,7 @@ class ASN1Integer extends ASN1Object {
 
     // the 64 bit integer is now in t[0]
     return t[0];
+    */
   }
 
   String toString() => "ASNInteger($intValue)";
