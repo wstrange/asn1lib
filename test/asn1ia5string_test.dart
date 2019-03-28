78c94d8b7d685465f57ac08cb8bce9d34fd17102
David Janes <david@consensas.com>
2019-03-28 06:31:49 -0400

    Added a legacy test case

--- test/asn1ia5string_test.dart
+++ test/asn1ia5string_test.dart
@@ -17,13 +17,41 @@ main() {
         0x00,
       ]);
     }
+  });
+  test("encode", () {
     // from https://docs.microsoft.com/en-us/windows/desktop/seccertenroll/about-IA5string
     {
       ASN1IA5String a = ASN1IA5String("Hello");
       expect(a.stringValue, "Hello");
 
       Uint8List encoded = a.encodedBytes;
-      expect(encoded, [0x16, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6F ]);
+      expect(encoded, [0x16, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6F]);
+    }
+  });
+  test("encode", () {
+    {
+      ASN1IA5String a = ASN1IA5String("test1@rsa.com");
+      expect(a.stringValue, "test1@rsa.com");
+
+      Uint8List encoded = a.encodedBytes;
+      expect(encoded, [
+        0x16,
+        0x0d,
+        0x74,
+        0x65,
+        0x73,
+        0x74,
+        0x31,
+        0x40,
+        0x72,
+        0x73,
+        0x61,
+        0x2e,
+        0x63,
+        0x6f,
+        0x6d,
+        0x00,
+      ]);
     }
   });
   test("decode", () {
@@ -44,6 +72,37 @@ main() {
       ]);
       ASN1IA5String a = ASN1IA5String.fromBytes(raw);
       expect(a.stringValue, "");
+    }
+    {
+      Uint8List raw =
+          Uint8List.fromList([0x16, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6F]);
+      ASN1IA5String a = ASN1IA5String.fromBytes(raw);
+      expect(a.stringValue, "Hello");
+    }
+  });
+
+  test("decode", () {
+    {
+      Uint8List raw = Uint8List.fromList([
+        0x16,
+        0x0d,
+        0x74,
+        0x65,
+        0x73,
+        0x74,
+        0x31,
+        0x40,
+        0x72,
+        0x73,
+        0x61,
+        0x2e,
+        0x63,
+        0x6f,
+        0x6d,
+        0x00,
+      ]);
+      ASN1IA5String a = ASN1IA5String.fromBytes(raw);
+      expect(a.stringValue, "test1@rsa.com");
     }
     {
       Uint8List raw =
