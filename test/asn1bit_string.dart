289bb6a90c269de5738eafc6ebf6b022cb7444ae
warren <warren.strange@gmail.com>
2022-09-14 16:54:50 -0600

    fix typo in bitstring test

--- test/asn1bit_string.dart
+++ test/asn1bit_string.dart
@@ -5,11 +5,11 @@ import 'package:asn1lib/asn1lib.dart';
 import 'dart:typed_data';
 
 void main() {
-  test("Decode 0 unused bits" () {
+  test("Decode 0 unused bits", () {
     final bytes = Uint8List.fromList([0x03, 0x02, 0x00, 0x20]);
     final bitString = ASN1BitString.fromBytes(bytes);
 
-    expect(bitString.unusedBits, 0);
+    expect(bitString.unusedbits, 0);
 
   });
 
@@ -17,6 +17,6 @@ void main() {
     final bytes = Uint8List.fromList([0x03, 0x02, 0x05, 0x80]);
     final bitString = ASN1BitString.fromBytes(bytes);
 
-    expect(bitString.unusedBits, 5);
+    expect(bitString.unusedbits, 5);
   });
 }
